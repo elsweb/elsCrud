ad0d78548cb23e20577f9eff3c208af1ba77071d
elsweb <atendimento@elsweb.com.br>
2017-07-02 11:43:16 -0300

    Aula 02 - Tratamento de erro de conexão

--- index.php
+++ index.php
@@ -6,4 +6,11 @@ $user = 'lab';
 $pass = '';
 $dbsa = 'estudo_pdocomphp';
 
+try {
 $Conn = new  \PDO("mysql:host={$host};dbname={$dbsa}","{$user}","{$pass}");//String DSN config
+}catch(\PDOException $e){
+	echo "<div style='padding:20px; background:black; color:red; width:200px; height:auto; margin: 0 auto; text-align:center;'>
+	<span>Não foi possivel estabelezer conexão com bando de dados <b>{$dbsa}</b></span><hr>
+	<span>Erro : <b>{$e->getCode()}</b></span>
+</div>";
+}
