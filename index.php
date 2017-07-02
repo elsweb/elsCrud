0ba6917bc00ed012cc1254464c712b98993f18ac
elsweb <atendimento@elsweb.com.br>
2017-07-02 11:45:41 -0300

    Aula 03 - Listagem de registros unicos e multiplos

--- index.php
+++ index.php
@@ -8,6 +8,17 @@ $dbsa = 'estudo_pdocomphp';
 
 try {
 $Conn = new  \PDO("mysql:host={$host};dbname={$dbsa}","{$user}","{$pass}");//String DSN config
+
+$query = "SELECT * FROM clientes";
+
+$stmt= $Conn->query($query); //Prepara para execução do script no banco de dados.
+$resultado = $stmt->fetch(PDO::FETCH_ASSOC); //Lista um array associativo [REGISTRO ÚNICO]
+//$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC); //Lista um array associativo [MULTIPLOS REGISTROS]
+
+echo'<pre>';
+print_r($resultado);
+echo'</pre>';
+
 }catch(\PDOException $e){
 	echo "<div style='padding:20px; background:black; color:red; width:200px; height:auto; margin: 0 auto; text-align:center;'>
 	<span>Não foi possivel estabelezer conexão com bando de dados <b>{$dbsa}</b></span><hr>
