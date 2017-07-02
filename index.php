4bb2a69b2de9e06115f0e8e873cedcacf759021a
elsweb <atendimento@elsweb.com.br>
2017-07-02 11:51:20 -0300

    Aula 04 - Listagem de registro

--- index.php
+++ index.php
@@ -9,12 +9,14 @@ $dbsa = 'estudo_pdocomphp';
 try {
 $Conn = new  \PDO("mysql:host={$host};dbname={$dbsa}","{$user}","{$pass}");//String DSN config
 
-//$query = "SELECT * FROM clientes";
-$query = "INSERT INTO clientes (nome, email) VALUES ('Emanuel','atendimento@elsweb.com.br')";
-$resultado = $Conn->exec($query);
+$query = "SELECT * FROM clientes";
 
-//$stmt= $Conn->query($query); //Prepara para execução do script no banco de dados.
-//$resultado = $stmt->fetch(PDO::FETCH_ASSOC); //Lista um array associativo [REGISTRO ÚNICO]
+//$query = "INSERT INTO clientes (nome, email) VALUES ('Emanuel','atendimento@elsweb.com.br')";
+//$resultado = $Conn->exec($query); // Executa a query no banco de dados
+
+$stmt= $Conn->query($query); //Prepara para execução do script no banco de dados.
+$resultado = $stmt->fetch(PDO::FETCH_ASSOC); //Lista um array associativo [REGISTRO ÚNICO]
+
 //$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC); //Lista um array associativo [MULTIPLOS REGISTROS]
 
 echo'<pre>';
