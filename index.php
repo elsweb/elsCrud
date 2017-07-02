ecc6bafc77417bcde7b7b736b9fa365483e1e7cb
elsweb <atendimento@elsweb.com.br>
2017-07-02 11:40:17 -0300

    Aula 01 - Conexão com banco de dados - MySQL

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
+
+$host = 'localhost';
+$user = 'lab';
+$pass = '';
+$dbsa = 'estudo_pdocomphp';
+
+$Conn = new  \PDO("mysql:host={$host};dbname={$dbsa}","{$user}","{$pass}");//String DSN config
