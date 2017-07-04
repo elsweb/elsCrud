3fc0c5b0c8dbc74556c5f6dba4b86d35f22509fe
elsweb <atendimento@elsweb.com.br>
2017-07-03 22:12:28 -0300

    Aula 05 - Listagem em laço de repedição.

--- index.php
+++ index.php
@@ -15,14 +15,15 @@ $query = "SELECT * FROM clientes";
 //$resultado = $Conn->exec($query); // Executa a query no banco de dados
 
 $stmt= $Conn->query($query); //Prepara para execução do script no banco de dados.
-$resultado = $stmt->fetch(PDO::FETCH_ASSOC); //Lista um array associativo [REGISTRO ÚNICO]
-
-//$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC); //Lista um array associativo [MULTIPLOS REGISTROS]
+//$resultado = $stmt->fetch(PDO::FETCH_ASSOC); //Lista um array associativo [REGISTRO ÚNICO]
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC); //Lista um array associativo [MULTIPLOS REGISTROS]
 //$resultado = $stmt->fetchAll(PDO::FETCH_CLASS); //Lista como objetos
 
-echo'<pre>';
-print_r($resultado);
-echo'</pre>';
+foreach ($resultado as $cliente) {
+	echo'<pre>';
+	print_r($cliente);
+	echo'</pre>';
+}
 
 }catch(\PDOException $e){
 	echo "<div style='padding:20px; background:black; color:red; width:200px; height:auto; margin: 0 auto; text-align:center;'>
