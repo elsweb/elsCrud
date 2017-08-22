ee1cb6161a6881d120d4d84b89bf9b12b3965cbd
elsweb <atendimento@elsweb.com.br>
2017-08-21 23:43:15 -0300

    php - more security for database

--- themes/rgcontact/php/action.php
+++ themes/rgcontact/php/action.php
@@ -1,33 +1,37 @@
 <?php
-sleep(1);
+//sleep(2);
 require_once('../../../_app/Config.inc.php');
 require_once('../../../_app/Conn/Conn.class.php');
 require_once('../../../_app/interface/EntidadeInterface.class.php');
 require_once('../../../_app/class/Client.class.php');
 require_once('../../../_app/sys/ServiceDb.class.php');
 
-$run = filter_input(INPUT_GET,'run',FILTER_DEFAULT);
 extract(filter_input_array(INPUT_POST,FILTER_DEFAULT));
 
 switch($run){
+	
 	case 'register' : 
-	$Conn = new Conn();
-	$conexao= $Conn->getConn();
+		$dados['nome'] = $txt_nome; //Reforçar segurança
+		$dados['email'] = $txt_email;
+		
+		if(!in_array('',$dados)):
+			$Conn = new Conn();
+		$Conexao = $Conn->getConn();
+		$Client = new Client();
+		$ServiceDb = new ServiceDb($Conexao, $Client);
+		$Client->setNameClient($dados['nome'])
+		->setEmailClient($dados['email']);
+		$Register = $ServiceDb->Create();
 
-	$Client = new Client();
-	$ServiceDb = new ServiceDb($conexao, $Client);
-
-	
-	$Client->setNameClient($txt_nome)
-	->setEmailClient($txt_email);
-	$resposta = $ServiceDb->Create();
-	
-	if($resposta):
-		echo "O registro <strong>{$txt_nome}</strong> foi efetuado com sucesso";
-		//print_r($txt_nome);
-	unset($Conn);
-	unset($Client);
-	unset($ServiceDb);
-	endif;	
-	break;
-}
+		if($Register):
+			$dados['jrtn'] = true;	
+		unset($Conn);
+		unset($Client);
+		unset($ServiceDb);
+		endif;
+		else:
+			$dados['jrtn'] = false;
+		endif;
+		echo json_encode($dados);
+		break;
+	}
