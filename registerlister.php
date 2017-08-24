2af14c5a3ec0662da7166d6690f987e5365d0a24
elsweb <atendimento@elsweb.com.br>
2017-08-23 23:45:39 -0300

    Created document for list the registers.

---
+++ registerlister.php
@@ -0,0 +1,34 @@
+<?php require_once('_app/Config.inc.php');?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Register Lister</title>
+	<link rel="stylesheet" href="<?=HOME?>/_cdn/bootstrap/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?=HOME?>/_cdn/boot/boot.css"/>
+</head>
+<body>
+	<div class="sm-content centered">
+		<div class="row">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>E-mail</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Emanuel</td>
+						<td>teste@teste.com.br</td>
+					</tr>
+					<tr>
+						<td>Aline</td>
+						<td>aline@teste.com.br</td>
+					</tr>					
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
