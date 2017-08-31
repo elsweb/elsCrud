439187080cdbe97f4811027a46a6aa6058050411
elsweb <atendimento@elsweb.com.br>
2017-08-31 13:46:55 -0300

    Update, incremental class for more security

---
+++ _app/Conn/ClassInterface.class.php
@@ -0,0 +1,9 @@
+<?php
+
+interface ClassInterface{
+	public function getId();
+	public function setId($id);
+
+	public function getTable();
+	public function setTable($table);
+}
--- _app/Conn/Read.class.php
+++ _app/Conn/Read.class.php
@@ -17,47 +17,57 @@ class Read extends Conn {
 	*Method for registers list on defined  fields 
 	*parameters for type array and string.
 	*@param type array field
-	*@param type string field
-	*use "," for separate indexes	
+	*@param type string field, use "," for separate indexes
+	*@param type string query, for Clauses	
 	*/
-	public function Read($field,$query = null){
-		if(is_array($field)):
-			$field = implode("," , $field);
-		$this->Query = "SELECT {$field} FROM `{$this->Table}` {$query}";
-		else:
-			$field = $field;
-		$this->Query = "SELECT {$field} FROM `{$this->Table}` {$query}";
-		endif;
+	public function Read($field = null,$query = null){
+		if(!is_null($field)):
+			if(is_array($field)):
+				$field = implode("," , $field);
+			$this->Query = "SELECT {$field} FROM `{$this->Table}` {$query}";
+			else:
+				$field = $field;
+			$this->Query = "SELECT {$field} FROM `{$this->Table}` {$query}";
+			endif;
+			else:
+				$this->Query = "SELECT * FROM `{$this->Table}` {$query}";
+			endif;
 		$conn = $this->setConnect($this->getConn()); //Connection
 		$stmt = $conn->query($this->Query); //Execute Query
 		$this->Return = $stmt->fetchAll(PDO::FETCH_ASSOC); // Data Return
-		return $this->Return;
-	}
-
-	/**
-	*<b>FullRead</b>
-	*Method for registers list all fields 
-	*parameters for type string.
-	*@param type string query
-	*/
-	public function FullRead($query = null){
-		$this->Query = "SELECT * FROM {$this->Table} {$query}";
-		$conn = $this->setConnect($this->getConn()); //Connection
-		$stmt = $conn->query($this->Query); //Execute Query
-		$this->Return = $stmt->fetchAll(PDO::FETCH_ASSOC); // Data Return
-		return $this->Return;
-	}
-
-	public function FindOut(){
 		
 	}
+	/**
+	*<b>FindOut</b>
+	*Method for found registers list on defined  fields 
+	*parameters for type array and string.
+	*@param type array field
+	*@param type string field, use "," for separate indexes
+	*@param type string query, for Clauses	
+	*/
+	public function FindOut($find, $field = null, $query = null){
+		if(!is_null($field)):
+			if(is_array($field)):
+				$field = implode("," , $field);
+			$this->Query = "SELECT {$field} FROM `{$this->Table}` WHERE id = :id";
+			else:
+				$field = $field;
+			$this->Query = "SELECT {$field} FROM `{$this->Table}` {$query}";
+			endif;
+			else:
+				$this->Query = "SELECT * FROM `{$this->Table}` WHERE id = :id";
+			endif;
+		$conn = $this->setConnect($this->getConn()); //Connection
+		$stmt = $conn->prepare($this->Query); //Execute Query
+		$stmt->bindValue(":id",$find);
+		$stmt->execute();
+		$this->Return = $stmt->fetch(PDO::FETCH_ASSOC); // Data Return
+		return $this->Return;
+	}
+
 	private function setConnect(PDO $conn){
 		$this->Connect = $conn;
 		return $this->Connect;
-	}
-
-	public function dm(){
-		return $this;
 	}
 }
 ?>
