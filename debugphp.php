a597a3863958ddc16dbe81f51fcbb2cbe341e80a
elsweb <atendimento@elsweb.com.br>
2017-08-31 13:43:34 -0300

    Increments Debug file for Create Class Refactoring

--- debugphp.php
+++ debugphp.php
@@ -1,12 +1,27 @@
 <?php
 require('_app/Config.inc.php');
 require('_app/Conn/Read.class.php');
+require('_app/Conn/Create.class.php');
+require('_app/class/Client.class.php');
 
-$dRead = new Read('clientes');
-$data  = array ('nome');
-//$full = $dRead->Read($data, 'ORDER BY nome');
-$full = $dRead->FullRead('ORDER BY nome');
-//$full = $dRead->dm();
+$cliente = new Client;
+$cliente->setNameClient('Emanuel');
+$cliente->setEmailClient('teste@teste.com.br');
+
+$create = new Create($cliente);
+$create->Create();
+
 echo '<pre>';
-print_r($full);
+print_r($create);
 echo '</pre>';
+
+//$dRead = new Read('clientes');
+//$dCreate = new Read('clientes');
+//$data  = array ('nome','email');
+//$full = $dRead->Read();
+//$full = $dRead->FindOut(231);
+//$full = $dRead->FullRead('ORDER BY nome');
+//$full = $dRead->dm();
+//echo '<pre>';
+//print_r($full);
+//echo '</pre>';
