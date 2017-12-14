ac76246b6ed41c6854280a586cda644fe15cc44a
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2017-12-14 19:52:53 -0200

    Update file

--- debugphp.php
+++ debugphp.php
@@ -5,29 +5,31 @@ require('_app/Conn/Create.class.php');
 require('_app/Conn/Update.class.php');
 require('_app/class/Client.class.php');
 
-$cliente = new Client;
+//$cliente = new Client;
 
 //$cliente->setIdClient(244);
-$cliente->setNameClient('Aline maria');
-$cliente->setEmailClient('teste@teste.com.br');
+//$cliente->setNameClient('Aline maria');
+//$cliente->setEmailClient('teste@teste.com.br');
 
 //$create = new Create($cliente);
 //$create->Create();
 
-$update = new Update($cliente);
-$update->Update(244);
+//$update = new Update($cliente);
+//$update->Update(244);
 
-echo '<pre>';
-print_r($update);
-echo '</pre>';
+//echo '<pre>';
+//print_r($update);
+//echo '</pre>';
 
-//$dRead = new Read('clientes');
+$dRead = new Read('clientes');
 //$dCreate = new Read('clientes');
 //$data  = array ('nome','email');
-//$full = $dRead->Read();
+$full = $dRead->Read();
 //$full = $dRead->FindOut(231);
 //$full = $dRead->FullRead('ORDER BY nome');
 //$full = $dRead->dm();
-//echo '<pre>';
-//print_r($full);
-//echo '</pre>';
+foreach ($full as $rtn) {
+	echo '<pre>';
+	print_r($rtn);
+	echo '</pre>';
+}
